583a034c4a57d9082b3ba0c1bf2d0fd445ab4919
codewithtaukeer <taukeerakbar.code@gmail.com>
2025-12-16 13:08:22 +0530

    Completed frontend almost and set the backend server next authentication karna hai

---
+++ client/src/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import View from './pages/View'
+import Pricing from './pages/Pricing'
+import Projects from './pages/Projects'
+import MyProjects from './pages/MyProjects'
+import Community from './pages/Community'
+import Preview from './pages/Preview'
+import Navbar from './components/Navbar'
+import path from 'path'
+
+const App = () => {
+const {pathname} =useLocation()
+const hideNavbar = pathname.startsWith('/projects/') &&pathname!== '/projects'
+                ||pathname.startsWith('/preview/')
+                ||pathname.startsWith('/view/')
+
+  return (
+    <div>
+      {
+        !hideNavbar && <Navbar/>
+      }
+      
+      <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/pricing' element={<Pricing/>} />
+          <Route path='/projects/:projectId' element={<Projects/>} />
+          <Route path='/projects' element={<MyProjects/>} />
+          <Route path='/preview/:projectId' element={<Preview/>} />
+          <Route path='/preview/:projectId/:versionId' element={<Preview/>} />
+          <Route path='/community' element={<Community/>} />
+          <Route path='/view/:projectId' element={<View/>} />
+          
+
+
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ server/server.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import { toNodeHandler } from 'better-auth/node';
+import { auth } from './lib/auth.js';
+
+const app = express();
+
+const corsOptions={
+    origin:process.env.TRUSTED_ORIGINS?.split(',')||[], 
+    credentials:true
+
+}
+
+app.use(cors())
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+const port = 3000;
+
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server is Live!');
+});
+  
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
